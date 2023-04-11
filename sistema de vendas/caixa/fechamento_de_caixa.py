a10a8b44dba0b1a62772ab40a3e2a9144bcf7b91
RayssaMoura <115087545+RayssaMoura@users.noreply.github.com>
2023-04-11 10:52:12 -0300

    Update fechamento_de_caixa.py

--- sistema de vendas/caixa/fechamento_de_caixa.py
+++ sistema de vendas/caixa/fechamento_de_caixa.py
@@ -1,14 +1,17 @@
+import time
 import PySimpleGUI as sg
 import bd.conect as ct
 conexao = ct.Connection()
 
 def fechamento_de_caixa():
+    caixa = False
+    tempo_aberto = []
     sg.theme('Default')
     sg.set_options(font=('Arial 12'), text_color='black')
 
     layout = [  
                 [sg.Text('Fechamento de Caixa', font=('Arial 16'))],
-                [sg.Button('Ver dias', size=(25,0))],
+                [sg.Button('Abrir Caixa', size=(25,0))],
                 [sg.Button('Voltar', size=(25,0))]
             ]
 
@@ -18,6 +21,20 @@ def fechamento_de_caixa():
         eventos, valores = janela.read()
         if eventos == sg.WIN_CLOSED or eventos == 'Voltar': 
             break
-        elif eventos == 'Ver dias':
-            pass
+
+        if caixa == False:
+            if eventos == 'Abrir Caixa':
+                if caixa == False:
+                    caixa = True
+                    sg.popup('Caixa Aberto')
+                    t = time.localtime(time.time())
+                    localtime = time.asctime(t)
+                    tempo_aberto.append(localtime)
+                    caixa = True
+        elif caixa == True:
+            if eventos == 'Abrir Caixa':
+                sg.popup('Caixa Fechado')
+                caixa = False
+
     janela.close()
+
